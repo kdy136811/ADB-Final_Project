8de1425a2c52e8f39fcf3f4315d89f388ba56993
kdy136811 <kdy136811@gmail.com>
2021-01-16 17:54:43 +0800

    combine with user_db

--- services/accounts_service.py
+++ services/accounts_service.py
@@ -12,13 +12,21 @@ def find_user(email: str):
     return user
 
 
-def create_user(name: str, email: str, company: str, password: str) -> Optional[User]:
+def create_user(username: str, name: str, email: str, affiliation: str, title: str, country: str, password: str) -> Optional[User]:
     if find_user(email):
         return None
     user = User()
+    max_id = graph.run(f"MATCH (n:user) RETURN ID(n) order by ID(n) DESC  LIMIT 1").data()
+    if len(max_id) == 0:
+        user.UID = '0'
+    else:
+        user.UID = str(max_id[0]['ID(n)']+1)
+    user.username = username
     user.name = name
     user.email = email
-    user.company = company
+    user.affiliation = affiliation
+    user.title = title
+    user.country = country
     user.hashed_password = hash_text(password)
     graph.create(user)
     return user
@@ -47,22 +55,24 @@ def login_user(email: str, password: str) -> Optional[User]:
 
 def get_profile(usr: str) -> Optional[User]:
     # user = User.match(graph, f"{usr}").first()
-    user_profile = graph.run(f"MATCH (x:user) WHERE x.email='{usr}' RETURN x.name as name, x.company as company, x.email as email").data()
+    user_profile = graph.run(f"MATCH (x:user) WHERE x.email='{usr}' RETURN x.username as username, x.name as name, x.email as email, x.affiliation as affiliation, x.title as title, x.country as country").data()
     return user_profile
 
-def update_profile(usr: str, name: str, company: str) -> Optional[User]:
+def update_profile(usr: str, username: str, name: str, affiliation: str, title: str, country: str) -> Optional[User]:
     # user = User.match(graph, f"{usr}").first()
-    user_profile = graph.run(f"MATCH (x:user) WHERE x.email='{usr}' SET x.name='{name}', x.company='{company}' RETURN x.name as name, x.company as company, x.email as email").data()
+    query = f"MATCH (x:user) WHERE x.email='{usr}' SET x.username='{username}', x.name='{name}', x.affiliation='{affiliation}', x.title='{title}', x.country='{country}'" \
+    "RETURN x.username as username, x.name as name, x.email as email, x.affiliation as affiliation, x.title as title, x.country as country"
+    user_profile = graph.run(query).data()
     return user_profile
 
 
 def find_user_equipment(usr: str):
     
-    count = graph.run("MATCH (x:user {email:$usr})-[:have_e]->(:equipments) return count(*)",usr=usr)
+    count = graph.run("MATCH (x:user {email:$usr})-[:have_e]->(:equipments) return count(*)",usr=usr).evaluate()
     print(count)
     return count
 
-def create_equipments(usr: str,eid: str ,Site: str,Longitude:float,Latitude:float,Altitude:float,tz:str,daylight:bool,wv: float,light_pollusion: float):
+def create_equipments(usr: str, eid: str, Site: str, Longitude: float, Latitude: float, Altitude: float, tz: str, daylight: bool, wv: float, light_pollusion: float):
 
     query = f"MATCH (x:user) where x.email='{usr}'  MATCH (e:equipments) where e.EID={eid}" \
     "CREATE (x)-[h:have_e{ site:$Site, longitude:$Longitude, latitude:$Latitude" \
